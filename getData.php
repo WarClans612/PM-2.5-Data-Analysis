a51cebb4102a4b6e89d2721f759908203cde8190
彭敬樺 <33000118+WarClans612@users.noreply.github.com>
2017-12-14 16:23:28 +0800

    Update getData.php

--- getData.php
+++ getData.php
@@ -6,10 +6,17 @@ $db_user = "user";
 $db_password = "pass";
 $dsn = "mysql:host=$db_host;dbname=$db_name;charset=utf8";
 $db = new PDO($dsn, $db_user, $db_password);
-$sql = "SELECT `id`, extract(hour from `time`) as Hourly, avg(`pm25`) as PM25 FROM `devices_data` WHERE `id` = :id AND `time` > DATE_SUB( NOW(), INTERVAL 1 DAY) GROUP BY `id`, extract(hour from `time`) ORDER BY ( Hourly + 24 - HOUR(NOW())) % 24";
+$sql = "SELECT `id`, extract(hour from `time`) as Hourly, avg(`pm25`) as PM25 FROM `devices_data` WHERE `id` = :id AND `time` > DATE_SUB( NOW(), INTERVAL 1 DAY) GROUP BY `id`, extract(hour from `time`)";
 $responseT = $db->prepare($sql);
 $id = $_POST['id'];
 $responseT->execute(array('id'=>$id));
+
+//Creating temporary array for hourly data
+$device_data = array(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0);
+$device_data_length = count($device_data);
+while($nt =  $responseT->fetch(PDO::FETCH_ASSOC)) {
+  $device_data[$nt['Hourly']] = $nt['PM25'];
+}
 
 //Creating json table that is ready to be used by Google Charts
 $table = array();
@@ -18,10 +25,11 @@ $table['cols'] = array (
   array('id' => "", 'label' => "PM 2.5", 'pattern' => "", 'type' => 'number')
 );
 $rows = array();
-while($nt =  $responseT->fetch(PDO::FETCH_ASSOC)) {
+for($i = 0; $i < $device_data_length; ++$i) {
   $temp = array();
-  $temp[] = array('v' => $nt['Hourly'], 'f' => NULL);
-  $temp[] = array('v' => $nt['PM25'], 'f' => NULL);
+  $wanted = ($i + (int)date('G')) % $device_data_length;
+  $temp[] = array('v' => $wanted, 'f' => NULL);
+  $temp[] = array('v' => $device_data[$wanted], 'f' => NULL);
   $rows[] = array('c' => $temp);
 }
 $table['rows'] = $rows;
